0d06e92fa2dc43ffebdbb160977774d992090c0b
GogeUVA <180322471+GogeUVA@users.noreply.github.com>
2026-06-02 11:51:04 -0400

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by George Yao on 6/2/26.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
